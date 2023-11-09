1f57a101fc48fecaf75920b5a243d4af619381f6
Hemanth_Kumar <phk.phk007@gmail.com>
2023-11-08 23:38:09 -0600

    Assignment done

--- controllers/games.js
+++ controllers/games.js
@@ -13,9 +13,24 @@ exports.games_list = async function (req, res) {
 exports.games_detail = function (req, res) {
   res.send("NOT IMPLEMENTED: Games detail: " + req.params.id);
 };
-// Handle games create on POST.
-exports.games_create_post = function (req, res) {
-  res.send("NOT IMPLEMENTED: Games create POST");
+// Handle Costume create on POST.
+exports.games_create_post = async function (req, res) {
+  console.log(req.body);
+  let document = new Games();
+  // We are looking for a body, since POST does not have query parameters.
+  // Even though bodies can be in many different formats, we will be picky
+  // and require that it be a json object
+  // {"game_name":"goat", "player_count":"12", "ticket_price":2000}
+  document.game_name = req.body.game_name;
+  document.player_count = req.body.player_count;
+  document.ticket_price = req.body.ticket_price;
+  try {
+    let result = await document.save();
+    res.send(result);
+  } catch (err) {
+    res.status(500);
+    res.send(`{"error": ${err}}`);
+  }
 };
 // Handle games delete form on DELETE.
 exports.games_delete = function (req, res) {
@@ -24,4 +39,16 @@ exports.games_delete = function (req, res) {
 // Handle games update form on PUT.
 exports.games_update_put = function (req, res) {
   res.send("NOT IMPLEMENTED: Games update PUT" + req.params.id);
+};
+
+// VIEWS
+// Handle a show all view
+exports.games_view_all_Page = async function (req, res) {
+  try {
+    theGames = await Games.find();
+    res.render("games", { title: "Games Search Results", results: theGames });
+  } catch (err) {
+    res.status(500);
+    res.send(`{"error": ${err}}`);
+  }
 };
